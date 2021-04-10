47bee90877e0c57504f308b1634be5cbdfa22bb4
MatthewLyons1056 <39568363+MatthewLyons1056@users.noreply.github.com>
2021-04-10 15:25:42 -0400

    Add files via upload

---
+++ Matthew/Matthew/MenuStart.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class MenuStart : MonoBehaviour
+{
+    private void Update()
+    {
+        if (CrossPlatformInputManager.GetButtonDown("begin"))
+        {
+            StartCoroutine(letsgo());
+            //SceneManager.LoadScene(1);
+        }
+        if (CrossPlatformInputManager.GetButtonDown("instructions"))
+        {
+            StartCoroutine(instruct());
+            //SceneManager.LoadScene(2);
+        }
+        if (CrossPlatformInputManager.GetButtonDown("menuBack"))
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+
+    IEnumerator letsgo()
+    {
+        AudioController.playButton3 = true;
+        yield return new WaitForSeconds(.5f);
+        SceneManager.LoadScene(1);
+    }
+
+    IEnumerator instruct()
+    {
+        AudioController.playButton3 = true;
+        yield return new WaitForSeconds(.5f);
+        SceneManager.LoadScene(2);
+    }
+}
---
+++ Matthew/Matthew/UIValues.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIValues : MonoBehaviour
+{
+    public int maxHappiness;
+    public int CurrentHappiness = 0;
+
+    public int happinessGain;
+
+    public Text happyText;
+
+    public Bar bar;
+
+    void Start()
+    {
+        //CurrentHappiness = maxHappiness;
+        //bar.SetMaxHealth(maxHappiness);
+    }
+
+    void Update()
+    {
+        happyText.text = "Happiness = " + CurrentHappiness;
+    }
+
+    public void GainRep()
+    {
+        CurrentHappiness = CurrentHappiness + happinessGain;
+
+        bar.SetHealth(CurrentHappiness);
+    }
+
+    public void Play()
+    {
+        GainRep();
+    }
+    public void Interact()
+    {
+        GainRep();
+    }
+    public void Feed()
+    {
+        GainRep();
+    }
+
+}
